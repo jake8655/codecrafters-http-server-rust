d29e6473dc19dd1284360dc9316f7105324f84f8
Jake <dominik8655@gmail.com>
2024-05-09 19:30:07 +0200

    maybe stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
-    println!("Logs from your program will appear here!");
+use anyhow::Result;
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
+    println!("Server started on: 127.0.0.1:4221");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: std::net::TcpStream) -> Result<()> {
+    let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+
+    Ok(())
 }
